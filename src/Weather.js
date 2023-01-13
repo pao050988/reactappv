ba479721143dad833d5c4b81092daa846439f1c2
pao050988 <pao050988@gmail.com>
2023-01-13 18:16:09 -0500

    added the real information
    the whole real information was added, everything is working, except the image and date.

--- src/Weather.js
+++ src/Weather.js
@@ -1,57 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
 import App from "./App.css";
+
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.css";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div class="row">
-          <div class="col-9">
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: "Friday 18:12",
+      description: response.data.weather[0].description,
+      iconUrl: "https//ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png",
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div class="row">
+            <div class="col-9">
+              <input
+                type="Search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+          </div>
+          <div class="col-3">
             <input
-              type="Search"
-              placeholder="Enter a city.."
-              className="form-control"
-              autoFocus="on"
+              type="submit"
+              value="Search"
+              className="btn btn-primary searchButton w-100"
             />
           </div>
-        </div>
-        <div class="col-3">
-          <input
-            type="submit"
-            value="Search"
-            className="btn btn-primary searchButton w-100"
-          />
-        </div>
-      </form>
-      <h1>New York, NY, USA</h1>
-      <ul>
-        <li>Friday 11:09 </li>
-        <li>Mostly Cloudy</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <div>
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="partlycloudy"
-            />
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
+        <div className="row">
+          <div className="col-6">
+            <div>
+              <img src={weatherData.iconUrl} alt={weatherData.description} />
+            </div>
+            <div>
+              <span className="temperature">
+                {Math.round(weatherData.temperature)}
+              </span>
+              <span className="unit">°C</span>
+            </div>
           </div>
-          <div>
-            <span className="temperature">6</span>
-            <span className="unit">°C</span>
-          </div>
-        </div>
 
-        <div className="col-6">
-          <ul>
-            <li>Precipitation:15%</li>
-            <li>Humidity:72%</li>
-            <li>Wind:13Km/h</li>
-          </ul>
+          <div className="col-6">
+            <ul>
+              <li>Humidity:{weatherData.humidity}%</li>
+              <li>Wind:{weatherData.wind}Km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = `45c635f6554467de219d60506c6e3432`;
+    let city = `new york`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading..";
+  }
 }
