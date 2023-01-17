d75069e27a5cd759d9b5491517d8e5e41cf6cbaf
pao050988 <pao050988@gmail.com>
2023-01-17 10:29:16 -0500

    added date

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import "./Weather.css";
 import App from "./App.css";
 
+import FormattedDate from "./FormattedDate";
+
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -13,7 +15,7 @@ export default function Weather(props) {
       ready: true,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
-      date: "Friday 18:12",
+      date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
       iconUrl: "https//ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png",
       wind: response.data.wind.speed,
@@ -44,7 +46,9 @@ export default function Weather(props) {
         </form>
         <h1>{weatherData.city}</h1>
         <ul>
-          <li>{weatherData.date}</li>
+          <li>
+            <FormattedDate date={weatherData.date} />
+          </li>
           <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row">
@@ -71,7 +75,7 @@ export default function Weather(props) {
     );
   } else {
     let apiKey = `45c635f6554467de219d60506c6e3432`;
-    
+
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
