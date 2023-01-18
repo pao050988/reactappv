13e2bf8c53d6b4cd2fb183a0f31544ecaea7c34b
pao050988 <pao050988@gmail.com>
2023-01-18 16:34:56 -0500

    added style,
    added style, background, fixed the organization mistakes

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -22,9 +22,8 @@ export default function WeatherTemperature(props) {
     return (
       <div className="WeatherTemperature">
         <span className="temperature">{Math.round(props.celsius)}</span>
-        <span className="unit">°C </span> |{" "}
-        <span>
-          {" "}
+        <span className="unit">°C {""}| </span>
+        <span className="unit">
           <a href="/" onClick={showFahrenheit}>
             °F
           </a>
@@ -36,12 +35,11 @@ export default function WeatherTemperature(props) {
       <div className="WeatherTemperature">
         <span className="temperature">{Math.round(fahrenheit())}</span>
         <span className="unit">
-          {" "}
           <a href="/" onClick={showCelsius}>
             °C
           </a>
+          | °F
         </span>
-        | <span className="unit">°F</span>
       </div>
     );
   }
--- src/Weatherinfo.js
+++ src/Weatherinfo.js
@@ -5,9 +5,8 @@ import WeatherTemperature from "./WeatherTemperature";
 
 export default function weatherinfo(props) {
   return (
-    <div className="weatherinfo">
+    <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
-
       <ul>
         <li>
           <FormattedDate date={props.data.date} />
@@ -17,21 +16,19 @@ export default function weatherinfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="d-flex">
-            <div className="float-left">
-              <div>
-                <Weathericon code={props.data.icon} size={65} />
-              </div>
-              <div>
-                <WeatherTemperature celsius={props.data.temperature} />
-              </div>
+            <div>
+              <Weathericon code={props.data.icon} size={70} />
+            </div>
+
+            <div>
+              <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
         </div>
-
         <div className="col-6">
           <ul>
-            <li>Humidity:{props.data.humidity}%</li>
-            <li>Wind:{props.data.wind}Km/h</li>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} km/h</li>
           </ul>
         </div>
       </div>
